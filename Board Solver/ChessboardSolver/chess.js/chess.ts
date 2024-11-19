bafd24703a37d117b68f6246fafaa787c7eea935
Jaswanth Duddu <57510269+jaswanthDuddu@users.noreply.github.com>
2024-11-19 16:47:53 -0500

    Create chess.ts

---
+++ Board Solver/ChessboardSolver/chess.js/chess.ts
@@ -0,0 +1,405 @@
+export const WHITE = 'w'
+export const BLACK = 'b'
+
+export const PAWN = 'p'
+export const KNIGHT = 'n'
+export const BISHOP = 'b'
+export const ROOK = 'r'
+export const QUEEN = 'q'
+export const KING = 'k'
+
+export type Color = 'w' | 'b'
+export type PieceSymbol = 'p' | 'n' | 'b' | 'r' | 'q' | 'k'
+
+// prettier-ignore
+export type Square =
+    'a8' | 'b8' | 'c8' | 'd8' | 'e8' | 'f8' | 'g8' | 'h8' |
+    'a7' | 'b7' | 'c7' | 'd7' | 'e7' | 'f7' | 'g7' | 'h7' |
+    'a6' | 'b6' | 'c6' | 'd6' | 'e6' | 'f6' | 'g6' | 'h6' |
+    'a5' | 'b5' | 'c5' | 'd5' | 'e5' | 'f5' | 'g5' | 'h5' |
+    'a4' | 'b4' | 'c4' | 'd4' | 'e4' | 'f4' | 'g4' | 'h4' |
+    'a3' | 'b3' | 'c3' | 'd3' | 'e3' | 'f3' | 'g3' | 'h3' |
+    'a2' | 'b2' | 'c2' | 'd2' | 'e2' | 'f2' | 'g2' | 'h2' |
+    'a1' | 'b1' | 'c1' | 'd1' | 'e1' | 'f1' | 'g1' | 'h1'
+
+export const DEFAULT_POSITION =
+  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+export type Piece = {
+  color: Color
+  type: PieceSymbol
+}
+
+type InternalMove = {
+  color: Color
+  from: number
+  to: number
+  piece: PieceSymbol
+  captured?: PieceSymbol
+  promotion?: PieceSymbol
+  flags: number
+}
+
+interface History {
+  move: InternalMove
+  kings: Record<Color, number>
+  turn: Color
+  castling: Record<Color, number>
+  epSquare: number
+  halfMoves: number
+  moveNumber: number
+}
+
+export type Move = {
+  color: Color
+  from: Square
+  to: Square
+  piece: PieceSymbol
+  captured?: PieceSymbol
+  promotion?: PieceSymbol
+  flags: string
+  san: string
+  lan: string
+  before: string
+  after: string
+}
+
+const EMPTY = -1
+
+const FLAGS: Record<string, string> = {
+  NORMAL: 'n',
+  CAPTURE: 'c',
+  BIG_PAWN: 'b',
+  EP_CAPTURE: 'e',
+  PROMOTION: 'p',
+  KSIDE_CASTLE: 'k',
+  QSIDE_CASTLE: 'q',
+}
+
+// prettier-ignore
+export const SQUARES: Square[] = [
+  'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+  'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+  'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+  'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+  'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+  'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+  'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+  'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+]
+
+const BITS: Record<string, number> = {
+  NORMAL: 1,
+  CAPTURE: 2,
+  BIG_PAWN: 4,
+  EP_CAPTURE: 8,
+  PROMOTION: 16,
+  KSIDE_CASTLE: 32,
+  QSIDE_CASTLE: 64,
+}
+
+const Ox88: Record<Square, number> = {
+  a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
+  a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
+  a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
+  a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
+  a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
+  a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
+  a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
+  a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
+}
+
+const PAWN_OFFSETS = {
+  b: [16, 32, 17, 15],
+  w: [-16, -32, -17, -15],
+}
+
+const PIECE_OFFSETS = {
+  n: [-18, -33, -31, -14, 18, 33, 31, 14],
+  b: [-17, -15, 17, 15],
+  r: [-16, 1, 16, -1],
+  q: [-17, -16, -15, 1, 17, 16, 15, -1],
+  k: [-17, -16, -15, 1, 17, 16, 15, -1],
+}
+
+// prettier-ignore
+const ATTACKS = [
+  20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20, 0,
+   0,20, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0,20, 0, 0,
+   0, 0,20, 0, 0, 0, 0, 24,  0, 0, 0, 0,20, 0, 0, 0,
+   0, 0, 0,20, 0, 0, 0, 24,  0, 0, 0,20, 0, 0, 0, 0,
+   0, 0, 0, 0,20, 0, 0, 24,  0, 0,20, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0,20, 2, 24,  2,20, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 2,53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+  24,24,24,24,24,24,56,  0, 56,24,24,24,24,24,24, 0,
+   0, 0, 0, 0, 0, 2,53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0,20, 2, 24,  2,20, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0,20, 0, 0, 24,  0, 0,20, 0, 0, 0, 0, 0,
+   0, 0, 0,20, 0, 0, 0, 24,  0, 0, 0,20, 0, 0, 0, 0,
+   0, 0,20, 0, 0, 0, 0, 24,  0, 0, 0, 0,20, 0, 0, 0,
+   0,20, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0,20, 0, 0,
+  20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20
+];
+
+// prettier-ignore
+const RAYS = [
+   17,  0,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0,  0, 15, 0,
+    0, 17,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 15,  0, 0,
+    0,  0, 17,  0,  0,  0,  0, 16,  0,  0,  0,  0, 15,  0,  0, 0,
+    0,  0,  0, 17,  0,  0,  0, 16,  0,  0,  0, 15,  0,  0,  0, 0,
+    0,  0,  0,  0, 17,  0,  0, 16,  0,  0, 15,  0,  0,  0,  0, 0,
+    0,  0,  0,  0,  0, 17,  0, 16,  0, 15,  0,  0,  0,  0,  0, 0,
+    0,  0,  0,  0,  0,  0, 17, 16, 15,  0,  0,  0,  0,  0,  0, 0,
+    1,  1,  1,  1,  1,  1,  1,  0, -1, -1,  -1,-1, -1, -1, -1, 0,
+    0,  0,  0,  0,  0,  0,-15,-16,-17,  0,  0,  0,  0,  0,  0, 0,
+    0,  0,  0,  0,  0,-15,  0,-16,  0,-17,  0,  0,  0,  0,  0, 0,
+    0,  0,  0,  0,-15,  0,  0,-16,  0,  0,-17,  0,  0,  0,  0, 0,
+    0,  0,  0,-15,  0,  0,  0,-16,  0,  0,  0,-17,  0,  0,  0, 0,
+    0,  0,-15,  0,  0,  0,  0,-16,  0,  0,  0,  0,-17,  0,  0, 0,
+    0,-15,  0,  0,  0,  0,  0,-16,  0,  0,  0,  0,  0,-17,  0, 0,
+  -15,  0,  0,  0,  0,  0,  0,-16,  0,  0,  0,  0,  0,  0,-17
+];
+
+const PIECE_MASKS = { p: 0x1, n: 0x2, b: 0x4, r: 0x8, q: 0x10, k: 0x20 }
+
+const SYMBOLS = 'pnbrqkPNBRQK'
+
+const PROMOTIONS: PieceSymbol[] = [KNIGHT, BISHOP, ROOK, QUEEN]
+
+const RANK_1 = 7
+const RANK_2 = 6
+/*
+ * const RANK_3 = 5
+ * const RANK_4 = 4
+ * const RANK_5 = 3
+ * const RANK_6 = 2
+ */
+const RANK_7 = 1
+const RANK_8 = 0
+
+const SIDES = {
+  [KING]: BITS.KSIDE_CASTLE,
+  [QUEEN]: BITS.QSIDE_CASTLE,
+}
+
+const ROOKS = {
+  w: [
+    { square: Ox88.a1, flag: BITS.QSIDE_CASTLE },
+    { square: Ox88.h1, flag: BITS.KSIDE_CASTLE },
+  ],
+  b: [
+    { square: Ox88.a8, flag: BITS.QSIDE_CASTLE },
+    { square: Ox88.h8, flag: BITS.KSIDE_CASTLE },
+  ],
+}
+
+const SECOND_RANK = { b: RANK_7, w: RANK_2 }
+
+const TERMINATION_MARKERS = ['1-0', '0-1', '1/2-1/2', '*']
+
+// Extracts the zero-based rank of an 0x88 square.
+function rank(square: number): number {
+  return square >> 4
+}
+
+// Extracts the zero-based file of an 0x88 square.
+function file(square: number): number {
+  return square & 0xf
+}
+
+function isDigit(c: string): boolean {
+  return '0123456789'.indexOf(c) !== -1
+}
+
+// Converts a 0x88 square to algebraic notation.
+function algebraic(square: number): Square {
+  const f = file(square)
+  const r = rank(square)
+  return ('abcdefgh'.substring(f, f + 1) +
+    '87654321'.substring(r, r + 1)) as Square
+}
+
+function swapColor(color: Color): Color {
+  return color === WHITE ? BLACK : WHITE
+}
+
+export function validateFen(fen: string) {
+  // 1st criterion: 6 space-seperated fields?
+  const tokens = fen.split(/\s+/)
+  if (tokens.length !== 6) {
+    return {
+      ok: false,
+      error: 'Invalid FEN: must contain six space-delimited fields',
+    }
+  }
+
+  // 2nd criterion: move number field is a integer value > 0?
+  const moveNumber = parseInt(tokens[5], 10)
+  if (isNaN(moveNumber) || moveNumber <= 0) {
+    return {
+      ok: false,
+      error: 'Invalid FEN: move number must be a positive integer',
+    }
+  }
+
+  // 3rd criterion: half move counter is an integer >= 0?
+  const halfMoves = parseInt(tokens[4], 10)
+  if (isNaN(halfMoves) || halfMoves < 0) {
+    return {
+      ok: false,
+      error:
+        'Invalid FEN: half move counter number must be a non-negative integer',
+    }
+  }
+
+  // 4th criterion: 4th field is a valid e.p.-string?
+  if (!/^(-|[abcdefgh][36])$/.test(tokens[3])) {
+    return { ok: false, error: 'Invalid FEN: en-passant square is invalid' }
+  }
+
+  // 5th criterion: 3th field is a valid castle-string?
+  if (/[^kKqQ-]/.test(tokens[2])) {
+    return { ok: false, error: 'Invalid FEN: castling availability is invalid' }
+  }
+
+  // 6th criterion: 2nd field is "w" (white) or "b" (black)?
+  if (!/^(w|b)$/.test(tokens[1])) {
+    return { ok: false, error: 'Invalid FEN: side-to-move is invalid' }
+  }
+
+  // 7th criterion: 1st field contains 8 rows?
+  const rows = tokens[0].split('/')
+  if (rows.length !== 8) {
+    return {
+      ok: false,
+      error: "Invalid FEN: piece data does not contain 8 '/'-delimited rows",
+    }
+  }
+
+  // 8th criterion: every row is valid?
+  for (let i = 0; i < rows.length; i++) {
+    // check for right sum of fields AND not two numbers in succession
+    let sumFields = 0
+    let previousWasNumber = false
+
+    for (let k = 0; k < rows[i].length; k++) {
+      if (isDigit(rows[i][k])) {
+        if (previousWasNumber) {
+          return {
+            ok: false,
+            error: 'Invalid FEN: piece data is invalid (consecutive number)',
+          }
+        }
+        sumFields += parseInt(rows[i][k], 10)
+        previousWasNumber = true
+      } else {
+        if (!/^[prnbqkPRNBQK]$/.test(rows[i][k])) {
+          return {
+            ok: false,
+            error: 'Invalid FEN: piece data is invalid (invalid piece)',
+          }
+        }
+        sumFields += 1
+        previousWasNumber = false
+      }
+    }
+    if (sumFields !== 8) {
+      return {
+        ok: false,
+        error: 'Invalid FEN: piece data is invalid (too many squares in rank)',
+      }
+    }
+  }
+
+  // 9th criterion: is en-passant square legal?
+  if (
+    (tokens[3][1] == '3' && tokens[1] == 'w') ||
+    (tokens[3][1] == '6' && tokens[1] == 'b')
+  ) {
+    return { ok: false, error: 'Invalid FEN: illegal en-passant square' }
+  }
+
+  // 10th criterion: does chess position contain exact two kings?
+  const kings = [
+    { color: 'white', regex: /K/g },
+    { color: 'black', regex: /k/g },
+  ]
+
+  for (const { color, regex } of kings) {
+    if (!regex.test(tokens[0])) {
+      return { ok: false, error: `Invalid FEN: missing ${color} king` }
+    }
+
+    if ((tokens[0].match(regex) || []).length > 1) {
+      return { ok: false, error: `Invalid FEN: too many ${color} kings` }
+    }
+  }
+
+  // 11th criterion: are any pawns on the first or eighth rows?
+  if (
+    Array.from(rows[0] + rows[7]).some((char) => char.toUpperCase() === 'P')
+  ) {
+    return {
+      ok: false,
+      error: 'Invalid FEN: some pawns are on the edge rows',
+    }
+  }
+
+  return { ok: true }
+}
+
+// this function is used to uniquely identify ambiguous moves
+function getDisambiguator(move: InternalMove, moves: InternalMove[]) {
+  const from = move.from
+  const to = move.to
+  const piece = move.piece
+
+  let ambiguities = 0
+  let sameRank = 0
+  let sameFile = 0
+
+  for (let i = 0, len = moves.length; i < len; i++) {
+    const ambigFrom = moves[i].from
+    const ambigTo = moves[i].to
+    const ambigPiece = moves[i].piece
+
+    /*
+     * if a move of the same piece type ends on the same to square, we'll need
+     * to add a disambiguator to the algebraic notation
+     */
+    if (piece === ambigPiece && from !== ambigFrom && to === ambigTo) {
+      ambiguities++
+
+      if (rank(from) === rank(ambigFrom)) {
+        sameRank++
+      }
+
+      if (file(from) === file(ambigFrom)) {
+        sameFile++
+      }
+    }
+  }
+
+  if (ambiguities > 0) {
+    if (sameRank > 0 && sameFile > 0) {
+      /*
+       * if there exists a similar moving piece on the same rank and file as
+       * the move in question, use the square as the disambiguator
+       */
+      return algebraic(from)
+    } else if (sameFile > 0) {
+      /*
+       * if the moving piece rests on the same file, use the rank symbol as the
+       * disambiguator
+       */
+      return algebraic(from).charAt(1)
+    } else {
+      // else use the file symbol
+      return algebraic(from).charAt(0)
+    }
+  }
+
+  return ''
+}
+
+//addMove
